1754dd41104468f8192f2e0cc2da1492a62dda97
Wadcawtgt12 <103714333+Wadcawtgt12@users.noreply.github.com>
2022-12-31 19:55:52 +0700

    Add files via upload

---
+++ plugins/info-script.js
@@ -0,0 +1,28 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let imgr = flaaa.getRandom()
+let text = `*Thanks To*
+    
+Allah SWT
+My parents
+All Friends
+All Contributors
+All Creator Bot
+Adiwajshing
+Nurutomo
+BochilGaming`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: 'Script di sini', url: 'Hayooo Mau Ngapain?'}},
+]
+let tm = {
+text: text,
+footer: global.bottime,
+templateButtons: templateButtons,
+image: `${imgr + 'Script'}`,
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['sc'] 
+ handler.tags = ['info'] 
+ handler.command = /^(s(ourcode|c))$/i 
+  
+ export default handler
